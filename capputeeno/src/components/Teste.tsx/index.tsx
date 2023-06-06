202f7f74a0609a53c2962f1fdfcdd6bb1774bcbf
Luis Carlos <lucaldev22@gmail.com>
2023-06-06 17:37:23 -0300

    More configs

---
+++ capputeeno/src/components/Teste.tsx/index.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react';
+import * as Styled from './styles';
+
+export default function Test() {
+  return (
+    <Styled.Container>
+      <h1>Olá mundo</h1>
+    </Styled.Container>
+  );
+}
