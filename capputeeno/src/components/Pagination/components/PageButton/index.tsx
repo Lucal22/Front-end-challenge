fdabd9aea1754487321503642688c2a2ed350190
Luis Carlos <lucaldev22@gmail.com>
2023-06-26 15:39:47 -0300

    Pagination pageButton component developed.

--- capputeeno/src/components/Pagination/components/PageButton/index.tsx
+++ capputeeno/src/components/Pagination/components/PageButton/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import * as Styled from '../../styles';
+import { useFilter } from '@/hooks/useFilter';
 
 export type PageButtonProps = {
   number: number;
 };
 
 export default function PageButton({ number }: PageButtonProps) {
-  return <Styled.PageButton isClicked={false}>{number}</Styled.PageButton>;
+  const { page, setPage } = useFilter();
+
+  function handlePageChange() {
+    setPage(number - 1);
+  }
+
+  return (
+    <Styled.PageButton
+      onClick={handlePageChange}
+      isClicked={page === number - 1}
+    >
+      {number}
+    </Styled.PageButton>
+  );
 }
