928c416a4f706518ee08e81777fe132801585cb7
Luis Carlos <lucaldev22@gmail.com>
2023-06-26 15:53:10 -0300

    Arrow buttons developed from pagination developed.

--- capputeeno/src/components/Pagination/components/PageArrows/index.tsx
+++ capputeeno/src/components/Pagination/components/PageArrows/index.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import * as Styled from '../../styles';
 import { ArrowLeft, ArrowRight } from '@/components/Icons';
+import { useFilter } from '@/hooks/useFilter';
+import useProductCounter from '@/hooks/useProductCounter';
 
 export default function PageArrows() {
-  return (
-    <>
-      <Styled.PageButton isClicked={false}>
-        <ArrowLeft />
-      </Styled.PageButton>
-      <Styled.PageButton isClicked={false}>
-        <ArrowRight />
-      </Styled.PageButton>
-    </>
-  );
+  const { page, setPage } = useFilter();
+  const { data } = useProductCounter();
+
+  if (data) {
+    return (
+      <>
+        <Styled.PageButton
+          onClick={() => setPage(page - 1)}
+          disabled={page === 0}
+          isClicked={false}
+        >
+          <ArrowLeft />
+        </Styled.PageButton>
+        <Styled.PageButton
+          onClick={() => setPage(page + 1)}
+          disabled={page === Number((data / 12 - 1).toFixed(0))}
+          isClicked={false}
+        >
+          <ArrowRight />
+        </Styled.PageButton>
+      </>
+    );
+  }
 }
