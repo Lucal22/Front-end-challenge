d1b804ce7b9f4b578a511bc7211c68652e21579b
Luis Carlos <lucaldev22@gmail.com>
2023-06-27 18:40:40 -0300

    Removing window from useLocalStorage

--- capputeeno/src/hooks/useLocalStorage.tsx
+++ capputeeno/src/hooks/useLocalStorage.tsx
@@ -4,11 +4,12 @@ export default function useLocalStorage<T>(item: string, initialValue: T) {
   const [value, setValue] = useState<T>(initialValue);
 
   useEffect(() => {
-    if (typeof window === 'undefined') return;
-    const value = localStorage.getItem(item);
-    if (value) setValue(JSON.parse(value));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [window]);
+    if (typeof window !== 'undefined') {
+      const value = localStorage.getItem(item);
+      if (value) setValue(JSON.parse(value));
+    }
+    return;
+  }, []);
 
   const postValue = (newValue: T) => {
     setValue(newValue);
